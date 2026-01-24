8103612342b85b9d8d03f9b0612ddfba9a0ca4d4
Exdip <109979785+Exdip@users.noreply.github.com>
2026-01-24 21:13:30 +0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,17 +1,8 @@
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-class Config:
-TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
-DEEPSEEK_API_KEY = os.getenv('DEEPSEEK_API_KEY')
-YOOKASSA_SHOP_ID = os.getenv('YOOKASSA_SHOP_ID')YOOKASSA_SECRET_KEY = os.getenv('YOOKASSA_SECRET_KEY')
-    
-    import os
-
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+YOOKASSA_SHOP_ID = os.getenv("YOOKASSA_SHOP_ID")
+YOOKASSA_SECRET_KEY = os.getenv("YOOKASSA_SECRET_KEY")
 YANDEX_API_KEY = os.getenv("YANDEX_API_KEY")
 YANDEX_FOLDER_ID = os.getenv("YANDEX_FOLDER_ID")
-
-    # Используем SQLite
 DB_PATH = os.getenv('DB_PATH', 'users.db')
